19d8c20aa22cb9ea520b9d3eb95ff6c03c2beae5
Tristan Carkuff <tristancarkuff@Tristans-MBP.frontierlocal.net>
2019-11-01 09:14:49 -0400

    got the data rendering the the screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      players:[]
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+    .then(res=> {
+      console.log(res)
+      //res.data
+      this.setState({
+        players: res.data
+      })
+    })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +39,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h2>Players</h2>
+        {this.state.players.map(coolPeople => (
+          <div key = 'coolPeople'>
+            <h4>{coolPeople.name}</h4>
+            <p>{coolPeople.country}</p>
+          </div>
+        ))}
     </div>
   );
+}
 }
 
 export default App;
